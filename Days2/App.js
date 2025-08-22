50109c2ddde0ba98632793af2472220fc26b5955
RamRaj110 <rampawankumar110@gmail.com>
2025-08-22 12:35:39 +0530

    React notes

---
+++ Days2/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading = React.createElement("h1",
+    {id:"helo"},
+    "Hello from React.",
+    React.createElement('div',{id:"desc"},"Helo i am div of Heading.")
+
+);
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
